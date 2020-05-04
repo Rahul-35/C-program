db84800e02c78d63b8e1b44df136acfb49dc6b79
Rahul Chakraborty <38361084+Rahul-35@users.noreply.github.com>
2020-05-04 11:12:35 +0530

    Add files via upload

---
+++ Basic/cversion.c
@@ -0,0 +1,14 @@
+//Program to get the C version
+#include<stdio.h>
+int main(int argc,char **argv)
+{
+	if( __STDC_VERSION__ >= 201710L) 
+		printf("We are using C18!\n");
+	else if (__STDC_VERSION__ >= 201112L)
+        	printf("We are using C11!\n");
+	else if (__STDC_VERSION__ >= 199901L) 
+        	printf("We are using C99!\n");
+	else 
+        	printf("We are using C89/C90!\n");
+	return(0);
+}
